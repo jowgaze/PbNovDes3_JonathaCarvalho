6d1a5e35373fdd7af13a795d3055a36776d8f715
jowgaze <jonatha.biomon.hp@gmail.com>
2025-01-21 21:05:30 -0300

    feat: criação do projeto spring para ms-event-management

---
+++ ms-event-management/src/main/java/br/com/compass/eventmanagement/MsEventManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.compass.eventmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEventManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEventManagementApplication.class, args);
+	}
+
+}
