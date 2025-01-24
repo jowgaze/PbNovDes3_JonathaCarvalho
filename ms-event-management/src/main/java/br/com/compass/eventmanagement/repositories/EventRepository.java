e4f4c1fe237a2f26b5209fa41c7bbc804dd3c4e6
jowgaze <jonatha.biomon.hp@gmail.com>
2025-01-24 13:47:06 -0300

    refactor: corrige o nome EventRespository para EventRepository

--- ms-event-management/src/main/java/br/com/compass/eventmanagement/repositories/EventRepository.java
+++ ms-event-management/src/main/java/br/com/compass/eventmanagement/repositories/EventRepository.java
@@ -3,5 +3,5 @@ package br.com.compass.eventmanagement.repositories;
 import br.com.compass.eventmanagement.domain.event.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface EventRespository extends MongoRepository<Event, String> {
+public interface EventRepository extends MongoRepository<Event, String> {
 }
