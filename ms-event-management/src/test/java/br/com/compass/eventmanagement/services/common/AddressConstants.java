d62575bb93c9f9897068d5e9e063ec573a71d153
jowgaze <jonatha.biomon.hp@gmail.com>
2025-01-24 13:49:24 -0300

    feat: implementa os testes unitários para AddressService

---
+++ ms-event-management/src/test/java/br/com/compass/eventmanagement/services/common/AddressConstants.java
@@ -0,0 +1,13 @@
+package br.com.compass.eventmanagement.services.common;
+
+import br.com.compass.eventmanagement.domain.address.Address;
+import br.com.compass.eventmanagement.dtos.viacep.ViaCepResponseDto;
+
+public class AddressConstants {
+    public static final Address ADDRESS_SAVED = new Address("01001-000", "Praça da Sé", "Sé", "São Paulo", "SP");
+    public static final ViaCepResponseDto VIACEP_RESPONSE = new ViaCepResponseDto(
+            "01001-000", "Praça da Sé", "lado ímpar", "",
+            "Sé", "São Paulo", "SP", "São Paulo", "Sudeste",
+            "3550308", "1004", "11", "7107", null
+    );
+}
