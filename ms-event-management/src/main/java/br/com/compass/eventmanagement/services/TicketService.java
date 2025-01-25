660551fce63a9e050a9579b4c1cdf41bf45507a5
jowgaze <jonatha.biomon.hp@gmail.com>
2025-01-25 12:58:42 -0300

    feat: adiciona exceções para deixar a documentação mais robusta

--- ms-event-management/src/main/java/br/com/compass/eventmanagement/services/TicketService.java
+++ ms-event-management/src/main/java/br/com/compass/eventmanagement/services/TicketService.java
@@ -1,17 +1,26 @@
 package br.com.compass.eventmanagement.services;
 
+import br.com.compass.eventmanagement.exceptions.TicketServiceException;
 import br.com.compass.eventmanagement.services.client.ticket.TicketClient;
 import br.com.compass.eventmanagement.dtos.event.HasTicketsDto;
+import feign.RetryableException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TicketService {
     private final TicketClient ticketClient;
 
     public boolean hasTickets(String eventId){
-        HasTicketsDto hasTickets = ticketClient.hasTickets(eventId);
-        return hasTickets.getHasTickets();
+        try {
+            HasTicketsDto hasTickets = ticketClient.hasTickets(eventId);
+            return hasTickets.getHasTickets();
+        } catch (RetryableException e){
+            log.error("Ticket service unavailable");
+            throw new TicketServiceException("ticket service unavailable");
+        }
     }
 }
