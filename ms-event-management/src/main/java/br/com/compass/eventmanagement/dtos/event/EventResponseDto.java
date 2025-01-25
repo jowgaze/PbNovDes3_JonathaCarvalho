b91e940281a93cca4db375f104ad6c36dfb55b89
jowgaze <jonatha.biomon.hp@gmail.com>
2025-01-25 13:05:03 -0300

    refactor: atualiza formato de data-hora das requisições para ISO 8601 YYYY-MM-DDThh:mm:ss

--- ms-event-management/src/main/java/br/com/compass/eventmanagement/dtos/event/EventResponseDto.java
+++ ms-event-management/src/main/java/br/com/compass/eventmanagement/dtos/event/EventResponseDto.java
@@ -1,7 +1,6 @@
 package br.com.compass.eventmanagement.dtos.event;
 
 import br.com.compass.eventmanagement.domain.address.Address;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -14,8 +13,6 @@ import java.time.LocalDateTime;
 public class EventResponseDto {
     private String id;
     private String name;
-
-    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime dateTime;
     private Address address;
 }
