c7984b49b26f36c7e73926f77b988a26a296aa9e
jowgaze <jonatha.biomon.hp@gmail.com>
2025-01-25 14:27:54 -0300

    feat: implementa a documentação para o microsserviço de ticket

--- ms-ticket-management/src/main/java/br/com/compass/ticketmanagement/controllers/TicketController.java
+++ ms-ticket-management/src/main/java/br/com/compass/ticketmanagement/controllers/TicketController.java
@@ -1,5 +1,6 @@
 package br.com.compass.ticketmanagement.controllers;
 
+import br.com.compass.ticketmanagement.controllers.exception.StandardError;
 import br.com.compass.ticketmanagement.domain.ticket.Ticket;
 import br.com.compass.ticketmanagement.dtos.event.HasTicketsDto;
 import br.com.compass.ticketmanagement.dtos.ticket.TicketRequestDto;
@@ -7,6 +8,11 @@ import br.com.compass.ticketmanagement.dtos.ticket.TicketResponseDto;
 import br.com.compass.ticketmanagement.dtos.ticket.TicketUpdateRequestDto;
 import br.com.compass.ticketmanagement.dtos.ticket.mapper.TicketMapper;
 import br.com.compass.ticketmanagement.services.TicketService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,9 +26,32 @@ import java.util.List;
 @RestController
 @RequestMapping("br/com/compass/ticketmanagement/v1")
 @RequiredArgsConstructor
+@Tag(name = "Tickets")
 public class TicketController {
     private final TicketService ticketService;
 
+
+    @Operation(
+            summary = "Create a new ticket",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Ticket created successfully",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TicketResponseDto.class))),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Event not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class))),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "Field validation error",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class))),
+                    @ApiResponse(
+                            responseCode = "503",
+                            description = "Event service unavailable",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class)))
+            }
+    )
     @PostMapping("/create-ticket")
     public ResponseEntity<TicketResponseDto> insert(@RequestBody @Valid TicketRequestDto request){
         log.info("Request to create ticket: {}", request);
@@ -32,6 +61,23 @@ public class TicketController {
         return ResponseEntity.status(201).body(response);
     }
 
+    @Operation(
+            summary = "Retrieve a ticket by ID",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Ticket retrieved successfully",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TicketResponseDto.class))),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Ticket or event not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class))),
+                    @ApiResponse(
+                            responseCode = "503",
+                            description = "Event service unavailable",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class)))
+            }
+    )
     @GetMapping("/get-ticket/{id}")
     public ResponseEntity<TicketResponseDto> findById(@PathVariable("id") String id){
         log.info("Request to get ticket. id: {}", id);
@@ -40,6 +86,23 @@ public class TicketController {
         return ResponseEntity.ok().body(response);
     }
 
+    @Operation(
+            summary = "Retrieve a tickets by CPF",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Tickets retrieved successfully",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TicketResponseDto.class))),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Ticket or event not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class))),
+                    @ApiResponse(
+                            responseCode = "503",
+                            description = "Event service unavailable",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class)))
+            }
+    )
     @GetMapping("/get-ticket-by-cpf/{cpf}")
     public ResponseEntity<List<TicketResponseDto>> findByCpf(@PathVariable("cpf") @CPF String cpf){
         log.info("Request to get tickets. cpf: {}", cpf);
@@ -48,6 +111,22 @@ public class TicketController {
         return ResponseEntity.ok().body(response);
     }
 
+    @Operation(
+            summary = "Update a ticket by ID",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Ticket updated successfully"),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Ticket not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class))),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "Field validation error",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class)))
+            }
+    )
     @PutMapping("/update-ticket/{id}")
     public ResponseEntity<Void> update(@PathVariable("id") String id, @RequestBody @Valid TicketUpdateRequestDto request){
         log.info("Request to update ticket. id: {}", id);
@@ -56,6 +135,18 @@ public class TicketController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "Cancel a ticket by ID (Soft delete)",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Ticket canceled successfully"),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Ticket not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class))),
+            }
+    )
     @DeleteMapping("/cancel-ticket/{id}")
     public ResponseEntity<Void> softDelete(@PathVariable("id") String id){
         log.info("Request to cancel ticket. id: {}", id);
@@ -64,6 +155,15 @@ public class TicketController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "Check if event has ticket",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Event checked successfully",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = HasTicketsDto.class)))
+            }
+    )
     @GetMapping("/check-tickets-by-event/{eventId}")
     public ResponseEntity<HasTicketsDto> hasTickets(@PathVariable("eventId") String eventId){
         log.info("Request to check if the event has a linked ticket. idEvent: {}", eventId);
