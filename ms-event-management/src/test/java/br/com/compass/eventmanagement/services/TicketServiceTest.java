9d49d852d8eea3112a165744852ba568c8f84492
jowgaze <jonatha.biomon.hp@gmail.com>
2025-01-27 17:44:30 -0300

    test: adiciona teste para a exceção TicketServiceException

--- ms-event-management/src/test/java/br/com/compass/eventmanagement/services/TicketServiceTest.java
+++ ms-event-management/src/test/java/br/com/compass/eventmanagement/services/TicketServiceTest.java
@@ -1,7 +1,9 @@
 package br.com.compass.eventmanagement.services;
 
 import br.com.compass.eventmanagement.dtos.event.HasTicketsDto;
+import br.com.compass.eventmanagement.exceptions.TicketServiceException;
 import br.com.compass.eventmanagement.services.client.ticket.TicketClient;
+import feign.RetryableException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,5 +32,14 @@ public class TicketServiceTest {
         boolean sut = service.hasTickets("eventId");
 
         assertThat(sut).isTrue();
+    }
+
+    @Test
+    public void hasTickets_EventHasTicket_ReturnsException(){
+        when(client.hasTickets("eventId")).thenThrow(RetryableException.class);
+
+        assertThatThrownBy(() -> service.hasTickets("eventId"))
+                .isInstanceOf(TicketServiceException.class)
+                .hasMessageContaining("ticket service unavailable");
     }
 }
