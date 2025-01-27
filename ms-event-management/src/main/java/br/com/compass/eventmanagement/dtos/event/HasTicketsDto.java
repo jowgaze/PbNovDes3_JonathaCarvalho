b44d7ff1723eca570029133a2736e067d6bf087a
jowgaze <jonatha.biomon.hp@gmail.com>
2025-01-27 17:25:14 -0300

    refactor: adiciona construtor em HasTicketDto com apenas (eventId, hasTicket)

--- ms-event-management/src/main/java/br/com/compass/eventmanagement/dtos/event/HasTicketsDto.java
+++ ms-event-management/src/main/java/br/com/compass/eventmanagement/dtos/event/HasTicketsDto.java
@@ -4,11 +4,16 @@ import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 @Setter
 @ToString
 public class HasTicketsDto {
+    @NonNull
     private String eventId;
+
+    @NonNull
     private Boolean hasTickets;
+
     private String error;
 }
