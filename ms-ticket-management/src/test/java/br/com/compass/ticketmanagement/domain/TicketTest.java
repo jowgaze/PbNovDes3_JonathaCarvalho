a43ca6f3ccb20aa181e69d5a1657e88645a27eba
jowgaze <jonatha.biomon.hp@gmail.com>
2025-01-27 22:40:15 -0300

    test: adiciona testes unitários para TicketMapper e para entidade Ticket

---
+++ ms-ticket-management/src/test/java/br/com/compass/ticketmanagement/domain/TicketTest.java
@@ -0,0 +1,32 @@
+package br.com.compass.ticketmanagement.domain;
+
+import br.com.compass.ticketmanagement.domain.ticket.Ticket;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@ExtendWith(SpringExtension.class)
+public class TicketTest {
+
+    @Test
+    public void ticketEquals() {
+        Ticket ticket1 = new Ticket();
+        Ticket ticket2 = new Ticket();
+        Ticket ticket3 = new Ticket();
+        ticket1.setId("1");
+        ticket2.setId("1");
+        ticket3.setId("3");
+
+        assertEquals(ticket1, ticket2);
+        assertNotEquals(ticket1, ticket3);
+        assertNotEquals(ticket1, new Object());
+        assertNotEquals(ticket2, null);
+        assertEquals(ticket1.hashCode(), ticket2.hashCode());
+        assertNotEquals(ticket1.hashCode(), ticket3.hashCode());
+    }
+}
