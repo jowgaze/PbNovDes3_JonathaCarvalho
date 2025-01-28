3d14f346220e72c1a425c2152e40b5413929fa07
jowgaze <jonatha.biomon.hp@gmail.com>
2025-01-28 11:16:51 -0300

    refactor: adiciona variáveis de ambiente para "host:port" na url do openfeign

--- ms-ticket-management/src/main/java/br/com/compass/ticketmanagement/services/client/EventClient.java
+++ ms-ticket-management/src/main/java/br/com/compass/ticketmanagement/services/client/EventClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-event", url = "localhost:8080/br/com/compass/eventmanagement/v1")
+@FeignClient(name = "ms-event", url = "${feign.event.url}/br/com/compass/eventmanagement/v1")
 public interface EventClient {
     @GetMapping("/get-event/{id}")
     EventResponseDto findById(@PathVariable("id") String id);
